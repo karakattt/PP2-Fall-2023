06f19908c9f15aab856fe0a90fc2e77f07d30a6b
Karakat Tilekbay <145279767+karakattt@users.noreply.github.com>
2023-09-25 23:26:36 +0600

    Lab# 2 commit
    Tilekbay Karakat lab2

---
+++ task1 find min num.py
@@ -0,0 +1,7 @@
+#task1 find min num
+x=int(input())
+y=int(input())
+if x>y:
+    print(y)
+else:
+    print(x)
---
+++ task6 do they match.py
@@ -0,0 +1,11 @@
+#task6 do they match?
+a=int(input())
+b=int(input())
+c=int(input())
+
+if a==b==c:
+    print(3)
+elif a==b or b==c or a==c:
+    print(2)
+elif a!=b and a!=c and b!=c:
+    print(0)
